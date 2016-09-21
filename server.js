1f096cdb02bdfa1ead42fdfceb9edcb5969666b2
sharmaravi0109 <beingravi1993@gmail.com>
2016-09-21 18:48:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/article-Two', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-Three', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
